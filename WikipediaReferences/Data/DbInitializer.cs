e80e89a8c1ad9bd27987206f19dff07c2259667b
Emiel Nijhuis <e.nijhuis@mmguide.nl>
2020-10-30 23:17:21 +0100

    completed task

---
+++ WikipediaReferences/Data/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System;
+
+namespace WikipediaReferences.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(WikipediaReferencesContext context)
+        {
+            SeedData seedData;
+
+            context.Database.EnsureCreated();
+
+            if (context.Sources.Any())
+                return;   // DB has been seeded
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Data", "SeedData.json");
+
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                seedData = JsonConvert.DeserializeObject<SeedData>(json);
+            }
+
+            foreach (var source in seedData.Sources)
+                context.Sources.Add(source);
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ WikipediaReferences/Migrations/20201030221529_InitialCreate.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WikipediaReferences.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sources",
+                columns: table => new
+                {
+                    Code = table.Column<string>(maxLength: 35, nullable: false),
+                    Name = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sources", x => x.Code);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "References",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 255, nullable: true),
+                    SourceCode = table.Column<string>(maxLength: 35, nullable: false),
+                    SomeInteger = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_References", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_References_Sources_SourceCode",
+                        column: x => x.SourceCode,
+                        principalTable: "Sources",
+                        principalColumn: "Code",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_References_SourceCode",
+                table: "References",
+                column: "SourceCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "References");
+
+            migrationBuilder.DropTable(
+                name: "Sources");
+        }
+    }
+}
