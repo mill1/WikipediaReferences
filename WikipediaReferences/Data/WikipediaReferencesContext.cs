7a05aff7f155b6f45ca022e388eb695f639f3f5d
Emiel Nijhuis <e.nijhuis@mmguide.nl>
2020-10-30 22:45:30 +0100

    completed task

---
+++ WikipediaReferences/Data/WikipediaReferencesContext.cs
@@ -0,0 +1,46 @@
+﻿//WikipediaReferencesContext
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WikipediaReferences.Models;
+
+namespace WikipediaReferences.Data
+{
+    public class WikipediaReferencesContext : DbContext
+    {
+        // TODO Models. (Attr)
+        public virtual DbSet<Models.Attribute> Attributes { get; set; }
+        public virtual DbSet<DataType> DataTypes { get; set; }
+
+        public WikipediaReferencesContext(DbContextOptions<WikipediaReferencesContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Attribute>(entity =>
+            {
+                entity.Property(e => e.DataTypeCode)
+                    .IsRequired()
+                    .HasMaxLength(35);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(255);
+            });
+
+            modelBuilder.Entity<DataType>(entity =>
+            {
+                entity.HasKey(e => e.Code);
+
+                entity.Property(e => e.Code)
+                    .HasMaxLength(35);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(255);
+            });
+        }
+    }
+}
---
+++ WikipediaReferences/Models/Attribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WikipediaReferences.Models
+{
+    public class Attribute
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DataTypeCode { get; set; }
+        public int SomeInteger { get; set; }
+    }
+}
