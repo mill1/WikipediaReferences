b7bc08ab00f133992b21c7cbaa2edd14dd2989c5
Emiel Nijhuis <e.nijhuis@mmguide.nl>
2020-11-10 22:35:01 +0100

    Completed task

--- WikipediaConsole/Startup.cs
+++ WikipediaConsole/Startup.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
-using System.Text;
 using WikipediaConsole.UI;
 using WikipediaReferences.Interfaces;
 using WikipediaReferences.Services;
